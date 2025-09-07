7f742664d4185a72d10956f443277ef00d52d3db
kingsuk <kingsuk055@gmail.com>
2025-09-07 20:57:37 +0530

    fix - error handling

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import { errorHandle } from './config/errorHandle';
+
+// handle unhandled Rejection by promises
+process.on('unhandledRejection', (reason, promise) => {
+  throw reason;
+});
+
+// unhandled error by try and catch block
+process.on('uncaughtException', (error: Error) => {
+  errorHandle.handleError(error);
+  if (!errorHandle.isTrustedError(error)) {
+    process.exit(1);
+  }
+});
+
 import express from 'express';
 import { centralizedErrorHandlerMiddlerWare } from './middlewares/centralizedErrorHandler';
 import userRoutes from './routes/userRoute';
